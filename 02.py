a7f5626d0e1841d2c56364afb5c982c65339b7e1
Anderson846 <anderson.ferreira2703@gmail.com>
2025-05-20 21:05:09 -0300

    Create 02.py

---
+++ 02.py
@@ -0,0 +1,5 @@
+#"2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+print("Anderson Roberto")
+print("rio de janeiro")
+print("Taquara")
+
